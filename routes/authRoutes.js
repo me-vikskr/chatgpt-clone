97b7c5a140dbc181a9d9a622a67ca4a47daf4a49
me-vikskr <vikashkr.bes33@gmail.com>
2023-08-26 18:33:32 +0530

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  registerContoller,
+  loginController,
+  logoutController,
+} = require("../controllers/authController.js");
+
+//router object
+const router = express.Router();
+
+//routes
+// REGISTER
+router.post("/register", registerContoller);
+
+//LOGIN
+router.post("/login", loginController);
+
+//LOGOUT
+router.post("/logout", logoutController);
+
+module.exports = router;
